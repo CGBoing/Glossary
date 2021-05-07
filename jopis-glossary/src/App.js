900f646b9900e20ef257ba34708f0f3b1cd1ea86
Jopi Mikkonen <jopi.mikkonen@iki.fi>
2021-05-07 10:56:57 +0300

    Basic tabbed interface.

--- jopis-glossary/src/App.js
+++ jopis-glossary/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Tabs from "./components/Tabs";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Glossaries of Stuff</h1>
+      <Tabs>
+        <div label="Statistics">
+          <h2>Basic statistical concepts</h2>
+          {/* <br />
+          <br /> */}
+          <i>Coming soon.</i>
+        </div>
+        <div label="Fallacies">
+          <h2>Logical fallacies in argumentation</h2>
+          These are the most common fallacius arguments you should <i>usually</i> avoid.
+          <br />
+          <br />
+          <i>Coming soon.</i>
+        </div>
+        <div label="About">
+          <h2>What am I looking at?</h2>
+          The motivations for creating this site are manifold: mainly to learn and revise Web app development techniques, React specifically.
+          <br />
+          Secondly, act as a glossary of things that might interest me at any given time - a so-called personal learning platform or reference, if you will.
+          <br />
+          Thirdly, it is sort of a UI/UX "test lab", so expect drastic changes and other whimsiness.
+          <br />
+          <br />
+          <i>Jopi Mikkonen</i>
+        </div>
+      </Tabs>
     </div>
   );
 }
